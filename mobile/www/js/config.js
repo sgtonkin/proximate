afb9fff2a307f6379f1ea2ece9e37aab870c0e1a
Sebastian Tonkin <sgtonkin@gmail.com>
2015-02-17 18:26:20 -0800

    Unhid client-side config files

---
+++ mobile/www/js/config.js
@@ -0,0 +1,12 @@
+// Usage: Copy this file to config.js and replace each value below.
+
+angular.module('proximate')
+
+.constant('pubNubKeys', {
+  sub: 'sub-c-55cc2d3c-8617-11e4-a77a-02ee2ddab7fe',
+  pub: 'pub-c-e3770297-47d1-4fe9-9c34-cfee91f9fa9c'
+})
+
+.constant('webServer', {
+	url: 'http://www.proximate.io'
+});
