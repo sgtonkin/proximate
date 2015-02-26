06f047a001e7ef93814b1e533e8cfa04ecd3f4d1
Derek Barncard <derek@barncard.com>
2015-02-25 16:45:49 -0800

    (fix/feat) added clearStorage() routine

--- mobile/www/js/services/localstorageFactory.js
+++ mobile/www/js/services/localstorageFactory.js
@@ -15,6 +15,9 @@ angular.module('proximate.services', [])
     },
     getObject: function(key) {
       return JSON.parse($window.localStorage[key] || '{}');
+    },
+    clearStorage: function() {
+      $window.localStorage.clear();
     }
   };
 }]);
